b5fdf2a8ef9d0ce8c28da0fa8079e0fea5973bc5
s-panatulag <s-panatulag@49S50024532.it.bsd405.org>
2019-06-06 13:37:38 -0700

    stopped infinite loop of note, need to make the note guide stay longer

--- src/ViolinTutorial.java
+++ src/ViolinTutorial.java
@@ -18,7 +18,7 @@ public class ViolinTutorial extends PApplet
 
     private int white;
     private int black;
-    private int color1;
+    private int red;
 
     private String pitch;
     private float delayInBeats;
@@ -46,7 +46,7 @@ public class ViolinTutorial extends PApplet
     public void setup() {
         white = color(255,255,255); // (r, g, b) [0, 256)
         black = color(0,0,0);
-        color1 = color(255,131,248);
+        red = color(255,0,0);
         
         minim = new Minim(this);
         out = minim.getLineOut( Minim.STEREO, 2048 );
@@ -62,7 +62,7 @@ public class ViolinTutorial extends PApplet
     public void draw() {
     	background(white); // Fill background color with white
         
-        fill(color1);
+        //fill(color1);
         fill(black);
         textSize(25);
         textAlign(CENTER, CENTER);
@@ -71,13 +71,12 @@ public class ViolinTutorial extends PApplet
         
         if (key == 't' || key == 'T') {
         	drawViolin();
-        	playTwinkle();
         } else if (key == 'x' || key == 'X') {
         	background(white);
-        	text("Goodbye!", 200, 200);
+         	text("Goodbye!", 200, 200);
         }
-              
         
+                 
         /*
         // Write pitch
         fill(black);
@@ -99,65 +98,70 @@ public class ViolinTutorial extends PApplet
         rect(120,40,5,300); // center x, center y, width, height
         String output = "G";
         text(output, 122, 360);
+        
         fill(black);
         rect(170,40,5,300); // center x, center y, width, height
         output = "D";
         text(output, 173, 360);
+        
         fill(black);
         rect(220,40,5,300); // center x, center y, width, height
         output = "A";
         text(output, 224, 360);
+        
         fill(black);
         rect(270,40,5,300); // center x, center y, width, height
         output = "E";
-        text(output, 273, 360);  
-        output ="Sridhar";
-        //Just testing
-    }
-    public void playTwinkle() {
-    	ellipse(200,200,200,100);
+        text(output, 273, 360);
     }
     
-    public void G3() {
-    	delayInBeats = 0;
-    	durationInBeats = 1;
-    	pitch = "G3";
-    	
-    	out.playNote(delayInBeats, durationInBeats, pitch);
+    public void playTwinkle() {
+    	A3();
+    }
+    
+    public void A3() {
+    	fill(red);
+    	noStroke();
+    	ellipse(123,110,40,40);
+        out.playNote(delayInBeats, durationInBeats, "A3");
     }
     /**
      * Handle Key Press (Down)
      */
     public void keyPressed() {
-        if(key == ' ') {
-            out.playNote(delayInBeats, durationInBeats, pitch);
-        }
-        else if(key == 'c') {
-            pitch = "C4";
-        }
-        else if(key == 'd') {
-        	pitch = "D4";
-        }
-        else if (key == '1') {
-        	durationInBeats += 0.5;
-        }
-        else if (key == '2' && durationInBeats >= 0.5) {
-        	durationInBeats -= 0.5;
-        }
-        else if (key == '4') {
-        	delayInBeats += 0.5;
-        }
-        else if (key == '5' && delayInBeats >= 0.5) {
-        	delayInBeats -= 0.5;
-        }
-        // TODO 1: If a user presses the 'd' key, change the pitch to "D4"
-        
-        // TODO 2: 
-        //    If a user presses the '1' key, increase the durationInBeats by 0.5f
-        //    If a user presses the '2' key, decrease the durationInBeats by 0.5f  (do not go below 0)
-        
-        // TODO 3: 
-        //    If a user presses the '4' key, increase the delayInBeats by 0.5f
-        //    If a user presses the '5' key, decrease the delayInBeats by 0.5f (do not go below 0)
+    	 if (key == 't' || key == 'T') {
+         	playTwinkle();
+         }
+    	 //    	 
+//        if(key == ' ') {
+//            out.playNote(delayInBeats, durationInBeats, pitch);
+//        }
+//        else if(key == 'c') {
+//            pitch = "C4";
+//        }
+//        else if(key == 'd') {
+//        	pitch = "D4";
+//        }
+//        else if (key == '1') {
+//        	durationInBeats += 0.5;
+//        }
+//        else if (key == '2' && durationInBeats >= 0.5) {
+//        	durationInBeats -= 0.5;
+//        }
+//        else if (key == '4') {
+//        	delayInBeats += 0.5;
+//        }
+//        else if (key == '5' && delayInBeats >= 0.5) {
+//        	delayInBeats -= 0.5;
+//        }
+//        // TODO 1: If a user presses the 'd' key, change the pitch to "D4"
+//        
+//        // TODO 2: 
+//        //    If a user presses the '1' key, increase the durationInBeats by 0.5f
+//        //    If a user presses the '2' key, decrease the durationInBeats by 0.5f  (do not go below 0)
+//        
+//        // TODO 3: 
+//        //    If a user presses the '4' key, increase the delayInBeats by 0.5f
+//        //    If a user presses the '5' key, decrease the delayInBeats by 0.5f (do not go below 0)
     }
 }
